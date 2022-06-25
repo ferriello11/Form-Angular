49f3ea32f03150866563c99bebec819d2695b8a1
Fabiano Lopes Faleiro <fabiano.lopes@conectt.com.br>
2022-06-25 04:25:28 -0300

    FIRST COMMIT

--- src/app/FORMULARIO/form/form.component.ts
+++ src/app/FORMULARIO/form/form.component.ts
@@ -16,14 +16,13 @@ export class FormComponent implements OnInit {
   constructor(private apiForm: ApiFormularioService) { }
 
   ngOnInit() {
-    // this.ListadosCadastrosForm();
-    this.apiForm.listar();
+    this.ListadosCadastrosForm();    
   }  
 
   CadastrarDadosFormulario(){  
     this.apiForm.postForm(this.formulario).subscribe(fomr =>{
       this.formulario =  new Formulario;
-      // this.ListadosCadastrosForm();
+      this.ListadosCadastrosForm();
     }, err =>{
       console.log('Erro ao cadastrar Aluno', err)
     })
@@ -31,62 +30,63 @@ export class FormComponent implements OnInit {
 
   ListadosCadastrosForm(){
     this.apiForm.listar().subscribe(forms =>{      
-        const letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-        var arrayAux = [];
-        var arryNomesAtuais = [];
-        var letraPorPosicao = [];
+        // const letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+        // var arrayAux = [];
+        // var arryNomesAtuais = [];
+        // var letraPorPosicao = [];
         
-        forms.map((usuario) => {
-          var nameForme = usuario.name.charAt();
-          arrayAux.push(letras.indexOf(nameForme.toUpperCase()))
-        });      
+        // forms.map((usuario) => {
+        //   var nameForme = usuario.name.charAt();
+        //   arrayAux.push(letras.indexOf(nameForme.toUpperCase()))
+        // });      
 
-        var ordernado = this.radixSort(arrayAux);     
+        // var ordernado = this.radixSort(arrayAux);     
         
-        for (let i = 0; i < forms.length; i++) {
-          var nameForme = forms[i].name.charAt();
-          nameForme.toUpperCase();
-          letraPorPosicao.push(letras[ordernado[i]]);
+        // for (let i = 0; i < forms.length; i++) {
+        //   var nameForme = forms[i].name.charAt();
+        //   nameForme.toUpperCase();
+        //   letraPorPosicao.push(letras[ordernado[i]]);
 
-          forms.map((usuario)=>{
-            if(usuario.name.charAt() == letraPorPosicao[i]){
-              arryNomesAtuais.push(usuario);                                    
-            }
-          });
+        //   forms.map((usuario)=>{
+        //     if(usuario.name.charAt() == letraPorPosicao[i]){
+        //       arryNomesAtuais.push(usuario);                                    
+        //     }
+        //   });
           
-          this.dados = arryNomesAtuais;
-          // this.dados = forms;
+        //   // this.dados = arryNomesAtuais;
+        //   this.dados = forms;
           
-        } 
+        // } 
+        this.dados = forms;
       },err =>{
         console.log('erro!',err);  
     })
   }
 
   
-  getMax(array){ // O(n)
-    let max = 0
-    for (let num of array) {
-      max = (max < num.toString().length) ? num.toString().length : max
-    }
-    return max
-  }
+  // getMax(array){ // O(n)
+  //   let max = 0
+  //   for (let num of array) {
+  //     max = (max < num.toString().length) ? num.toString().length : max
+  //   }
+  //   return max
+  // }
 
-  getPosition(num,place){
-   return Math.floor(num / Math.pow(10,place)) % 10;
-  } 
+  // getPosition(num,place){
+  //  return Math.floor(num / Math.pow(10,place)) % 10;
+  // } 
 
-  radixSort(array){ // O(nk)
-    var max = this.getMax(array)
-    for (let i = 0; i < max; i++) {
-      let buckets = Array.from({length:10}, () => [])
-      for (let j = 0; j < array.length; j++) {        
-        buckets[this.getPosition(array[j], i)].push(array[j])
-      }
-      array = [].concat(...buckets)
-    }
-    return array
-  }
+  // radixSort(array){ // O(nk)
+  //   var max = this.getMax(array)
+  //   for (let i = 0; i < max; i++) {
+  //     let buckets = Array.from({length:10}, () => [])
+  //     for (let j = 0; j < array.length; j++) {        
+  //       buckets[this.getPosition(array[j], i)].push(array[j])
+  //     }
+  //     array = [].concat(...buckets)
+  //   }
+  //   return array
+  // }
 
 }
     
